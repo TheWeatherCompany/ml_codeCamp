016465c7708c142f050179fc4a9498edfee0bf99
maxjacu <max.jacubowsky@ibm.com>
2018-06-06 10:12:00 -0400

    added working ml code

--- week1/machineLearning.py
+++ week1/machineLearning.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python
 import os
 import pyarrow.parquet as pq
-import sklearn
-#This script requires 'dataExploration.py' to run before to unzip our data
+## sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+## Pre-requesites:
+## This script requires 'dataExploration.py' to run before to unzip our data
 folder = '/data/'
 filename = 'TopVideoSmall'
 
 ## Load data as pandas dataframe using pyArrow
 dS = pq.ParquetDataset(os.getcwd() + folder + filename)
-df = dS.read().to_pandas()
+df = (dS.read()
+      .to_pandas()
+      .select_dtypes(exclude=['object', 'long', 'datetime64'])
+      .drop_duplicates()
+      .reset_index()
+      )
 
 ### Data Preparation
 ## Renaming column names for tile related metrics
@@ -60,4 +71,35 @@ for metric in ['_r', '_vi', '_cl']:
          [x + metric for x in allTiles]
         )
     ), inplace = True)
-print
+
+X = df[[x for x in df.columns        # Features
+        if x != 'label']].fillna(0)  #We can't train a model with empty cells in our datafram
+Y = df['label'].values.astype(int)   # Label
+# Split Data and Train Model
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=100)
+
+# Define Classifier
+rfc = RandomForestClassifier(criterion="gini",
+                             random_state=100,
+                             max_depth=10,
+                             n_estimators=10,  # Number of Trees
+                             min_samples_leaf=8,  # Percentage of Sample Size
+                             min_samples_split=20)
+
+rfc.fit(X_train, y_train)
+
+# Model Metrics
+y_pred = rfc.predict(X_test)
+print " Accuracy is ", accuracy_score(y_test, y_pred) * 100
+print " Confusion Matrix:", confusion_matrix(y_test, y_pred)
+
+
+##Print Feature Importance
+## Filters out features with low importance
+featureNames = df[[x for x in df.columns if x != 'label']].columns
+j = 0
+fi =  rfc.feature_importances_
+for i in featureNames:
+    if fi[j] >= fi.mean():
+        print i, fi[j]
+    j += 1
